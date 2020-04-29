7812558a3988533aee81cf39388a48b52fa54245
Tunde Adepitan <tunde262@gmail.com>
2020-04-29 14:05:41 -0500

    finishing up

--- config/keys_prod.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongoURI: process.env.MONGO_URI,
-    jwtSecret: process.env.JWT_SECRET,
-    githubClientId: process.env.GITHUB_CLIENT_ID,
-    githubSecret: process.env.GITHUB_SECRET,
-};
